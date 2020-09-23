b565ca931b1b5f21252cb0823ccd439f8ca4a57e
DamianBrdej <brdej98@gmail.com>
2020-09-23 15:34:30 +0200

    v1

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+function zeroPad(number) {
+    if (number.toString(10).length == 1) {
+        return "0" + number;
+    }
+    else {
+        return number;
+    }
+}
+
+function getCurrentTime() {
+    let currentTime = new Date();
+    return {
+        hour: currentTime.getHours(),
+        minute: currentTime.getMinutes(),
+        second: currentTime.getSeconds()
+    }
+}
+
+function displayTime() {
+    let time = getCurrentTime();
+    
+    let decimalTimeElement = document.getElementById("decimal-time");
+    
+    decimalTimeElement.innerHTML = "#" + zeroPad(time.hour) + zeroPad(time.minute) + zeroPad(time.second);
+    document.body.style["background-color"] = "#" + zeroPad(time.hour) + zeroPad(time.minute) + zeroPad(time.second);
+}
+
+setInterval(displayTime, 1000);
+
